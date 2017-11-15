a1004e0ab22dffe9d58cdc594a1e1f177b17689f
harish <harishchopra86@gmail.com>
2017-11-15 18:15:35 -0500

    assets added ui created custom text field class crested for currency field

---
+++ Shopper/Shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyTextField.swift
+//  Shopper
+//
+//  Created by Harish Chopra on 2017-11-15.
+//  Copyright © 2017 Harish Chopra. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2474141725)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        if let place = placeholder {
+            let place = NSAttributedString(string: place, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+        }
+    }
+}
