5f509d6239ad747da46d3ad56b228b4edbbf91e7
harish <harishchopra86@gmail.com>
2017-11-15 18:35:39 -0500

    custom text fields added Ibdesignable used input accessory view added to text fields

--- Shopper/Shopper/Views/CurrencyTextField.swift
+++ Shopper/Shopper/Views/CurrencyTextField.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
-
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2474141725)
-        layer.cornerRadius = 5.0
-        textAlignment = .center
-        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        
-        if let place = placeholder {
-            let place = NSAttributedString(string: place, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-            attributedPlaceholder = place
-        }
+        customizeView()
     }
+
+func customizeView() {
+    backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2474141725)
+    layer.cornerRadius = 5.0
+    textAlignment = .center
+    textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    
+    if let place = placeholder {
+        let place = NSAttributedString(string: place, attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        attributedPlaceholder = place
+    }
+    
+}
 }
